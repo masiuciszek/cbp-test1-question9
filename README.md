0abe02e59ea807c2433dd38b2bc08ee116238466
Masiu <ciszekmarcell@gmail.com>
2020-10-17 14:21:14 +0200

    graphql server addon

---
+++ README.md
@@ -0,0 +1,10 @@
+# server-graphql
+
+## Table of Contents
+
+- [About](#about)
+
+## About <a name = "about"></a>
+
+Graphql server, a simple graphql server.
+Nice boilerplate for how you can keep building on it.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("asdasd")
+import { ApolloServer } from "apollo-server"
+import gql from "graphql-tag"
+
+const typeDefs = gql`
+  type User {
+    email: String! #Not null
+    name: String!
+    friends: [User]! #array should not be null
+  }
+
+  type Query {
+    me: User!
+  }
+`
+
+const resolvers = {
+  Query: {
+    me() {
+      return {
+        email: "someemail@example.com",
+        name: "Mike smith",
+        friends: [],
+      }
+    },
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen(4000).then(() => console.log(`server is on port 4000 🚀`))
